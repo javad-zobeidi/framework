f84abf51080338a87abcb42e4dd267feb23d7012
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-12-25 12:08:01 +0330

    Add session management (#152)

---
+++ lib/src/http/session/session_file_store .dart
@@ -0,0 +1,111 @@
+// ignore_for_file: unintended_html_in_doc_comment, file_names
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
+import 'package:vania/vania.dart';
+
+import '../../cryptographic/vania_encryption.dart';
+
+class SessionFileStore {
+  static final SessionFileStore _singleton = SessionFileStore._internal();
+  factory SessionFileStore() => _singleton;
+  SessionFileStore._internal();
+
+  final String _secretKey = env('APP_KEY');
+
+  final String sessionPath = 'storage/framework/sessions';
+
+  /// Stores a session in the file system. The session is stored in the sessionPath directory,
+  /// and the filename is the hash of the given sessionId. The session data is encrypted
+  /// using the APP_KEY from the environment.
+  ///
+  /// The session data is stored in the file with the following format:
+  /// {
+  ///   "data": <the session data>,
+  ///   "expiration": <the expiration time in milliseconds>
+  /// }
+  ///
+  /// The expiration time is calculated from the current UTC time plus the given duration.
+  /// The default duration is 1 hour.
+  ///
+  /// The method is asynchronous and returns a Future<void>.
+  void storeSession(String sessionId, Map<String, dynamic> data,
+      {Duration duration = const Duration(hours: 1)}) async {
+    sessionId = _makeHash(sessionId).toString();
+    final file = File('$sessionPath/$sessionId');
+
+    if (!file.existsSync()) {
+      file.createSync(recursive: true);
+    }
+
+    int expiration =
+        DateTime.now().toUtc().millisecondsSinceEpoch + duration.inMilliseconds;
+    await file.writeAsString(VaniaEncryption.encryptString(
+        json.encode({"data": data, "expiration": expiration}), _secretKey));
+  }
+
+  /// Retrieves a session from the file system. The session is retrieved from the sessionPath directory,
+  /// and the filename is the hash of the given sessionId. The session data is decrypted
+  /// using the APP_KEY from the environment.
+  ///
+  /// The method is synchronous and returns a Map<String, dynamic> containing the session data.
+  /// If the session has expired, the method deletes the file and returns null.
+  Map<String, dynamic>? retrieveSession(String sessionId) {
+    sessionId = _makeHash(sessionId).toString();
+    final file = File('$sessionPath/$sessionId');
+
+    if (!file.existsSync()) {
+      return null;
+    }
+
+    Map<String, dynamic> data = json.decode(
+        VaniaEncryption.decryptString(file.readAsStringSync(), _secretKey));
+    int expiration = data['expiration'].toString().toInt() ?? 0;
+    if (!DateTime.now()
+        .toUtc()
+        .isBefore(DateTime.fromMillisecondsSinceEpoch(expiration))) {
+      file.deleteSync();
+      return null;
+    }
+    return data['data'];
+  }
+
+  /// Checks if a session exists in the file system, and if it has not expired.
+  ///
+  /// The method is synchronous and returns a boolean indicating if the session exists and is valid.
+  /// If the session has expired, the method deletes the file and returns false.
+  bool hasSession(String sessionId) {
+    sessionId = _makeHash(sessionId).toString();
+    final file = File('$sessionPath/$sessionId');
+    if (!file.existsSync()) {
+      return false;
+    }
+    Map<String, dynamic> data = json.decode(
+        VaniaEncryption.decryptString(file.readAsStringSync(), _secretKey));
+    int expiration = data['expiration'].toString().toInt() ?? 0;
+    if (!DateTime.now()
+        .toUtc()
+        .isBefore(DateTime.fromMillisecondsSinceEpoch(expiration))) {
+      file.deleteSync();
+      return false;
+    }
+    return true;
+  }
+
+  void deleteSession(String sessionId) {
+    sessionId = _makeHash(sessionId).toString();
+    final file = File('$sessionPath/$sessionId');
+    if (file.existsSync()) {
+      file.deleteSync();
+    }
+  }
+
+  Digest _makeHash(String key) {
+    var secKey = utf8.encode(_secretKey);
+    var bytes = utf8.encode(key);
+    var hmacSha256 = Hmac(sha256, secKey);
+    return hmacSha256.convert(bytes);
+  }
+}
---
+++ lib/src/http/session/session_manager.dart
@@ -0,0 +1,149 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+import 'session_file_store .dart';
+
+class SessionManager {
+  static final SessionManager _instance = SessionManager._internal();
+  factory SessionManager() => _instance;
+  SessionManager._internal();
+
+  HttpRequest? _request;
+
+  final Duration _sessionTimeout = const Duration(seconds: 3600);
+
+  final Random _random = Random.secure();
+
+  /// Generates a new session ID.
+  ///
+  /// This method creates a 64-byte random key using a secure random number generator,
+  /// and encodes it using base64 URL encoding to ensure a unique and safe session ID.
+  ///
+  /// Returns:
+  /// A base64 URL encoded string representing the session ID.
+  String _generateSessionId() {
+    final keyBytes = List<int>.generate(64, (_) => _random.nextInt(256));
+    return base64Url.encode(keyBytes);
+  }
+
+  /// Starts a new session or retrieves an existing session ID from the request.
+  ///
+  /// This method initializes a session for the given HTTP request and response.
+  /// If a 'SESSION_ID' cookie is already present in the request, its value is
+  /// used as the session ID. Otherwise, a new session ID is generated and set
+  /// as a cookie in the response.
+  ///
+  /// The session ID is stored in a cookie with properties configured for HTTP
+  /// only access, insecure transmission (consider changing to true for secure
+  /// transmission), a path set to '/', and an expiration set to the session
+  /// timeout duration.
+  ///
+  /// Parameters:
+  /// - [request]: The incoming HTTP request containing cookies.
+  /// - [response]: The HTTP response where the session cookie will be added.
+  ///
+  /// Returns:
+  /// A string representing the session ID.
+  String sessionStart(HttpRequest request, HttpResponse response) {
+    _request = null;
+
+    _request ??= request;
+
+    final cookie = request.cookies.firstWhere(
+      (c) => c.name == 'SESSION_ID',
+      orElse: () => Cookie('SESSION_ID', _generateSessionId()),
+    );
+    String sessionId = cookie.value;
+    response.cookies.add(
+      Cookie('SESSION_ID', sessionId)
+        ..httpOnly = true
+        ..secure = false
+        ..path = '/'
+        ..expires = DateTime.now().add(_sessionTimeout),
+    );
+
+    return sessionId;
+  }
+
+  String? getSessionId() {
+    final cookie = _request?.cookies.firstWhere(
+      (c) => c.name == 'SESSION_ID',
+      orElse: () => Cookie('SESSION_ID', ''),
+    );
+    return cookie?.value;
+  }
+
+  /// Retrieves all session data associated with the current session ID.
+  ///
+  /// This function checks if there is an active session by retrieving the
+  /// session ID. If a session ID is found, it verifies the existence and
+  /// validity of the session. If the session exists, it retrieves and returns
+  /// the session data as a map. If the session does not exist or is invalid,
+  /// it returns null.
+  ///
+  /// Returns:
+  /// A map containing the session data if a valid session exists, otherwise
+  /// returns null.
+  Map<String, dynamic>? getAllSessions() {
+    final sessionId = getSessionId();
+    if (sessionId != null) {
+      if (!SessionFileStore().hasSession(sessionId)) {
+        return null;
+      }
+      Map<String, dynamic> session = {};
+
+      session = SessionFileStore().retrieveSession(sessionId) ?? {};
+      return session;
+    }
+    return null;
+  }
+
+  dynamic getSession(String key) {
+    Map<String, dynamic>? session = getAllSessions();
+    return session?[key];
+  }
+
+  /// Stores a value in the session data associated with the current session ID.
+  ///
+  /// If a session ID is found, it verifies the existence and validity of the session.
+  /// If the session exists, it updates the session data by adding the given key-value pair,
+  /// and saves the updated session data. If the session does not exist or is invalid,
+  /// it does not store the value.
+  ///
+  void setSession(String key, dynamic value) {
+    final sessionId = getSessionId();
+    if (sessionId != null) {
+      Map<String, dynamic> session =
+          SessionFileStore().retrieveSession(sessionId) ?? {};
+      session.addAll({key: value});
+      SessionFileStore().storeSession(sessionId, session);
+    }
+  }
+
+  /// Deletes a specific key from the current session data.
+  ///
+  /// If a session ID is found, it verifies the existence and validity of the session.
+  /// If the session exists, it removes the given key from the session data, and saves the
+  /// updated session data. If the session does not exist or is invalid, it does not delete
+  /// the key.
+  ///
+  /// Parameters:
+  /// - [key]: The key to be deleted from the session data.
+  void deleteSessionKey(String key) {
+    final sessionId = getSessionId();
+    if (sessionId != null) {
+      Map<String, dynamic> session =
+          SessionFileStore().retrieveSession(sessionId) ?? {};
+      session.remove(key);
+      SessionFileStore().storeSession(sessionId, session);
+    }
+  }
+
+  void destroySession() {
+    final sessionId = getSessionId();
+    if (sessionId != null) {
+      SessionFileStore().deleteSession(sessionId);
+    }
+  }
+}
